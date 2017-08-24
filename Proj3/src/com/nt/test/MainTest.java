4f65b9ada605bf4413f5c12a72da50164f808435
Shashank <Shashank@TheDevil>
2017-08-25 00:42:55 +0530

    edited by prog1

--- Proj3/src/com/nt/test/MainTest.java
+++ Proj3/src/com/nt/test/MainTest.java
@@ -5,6 +5,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("main class");
+		System.out.println("edited by prog1");
 	}
 
 }
